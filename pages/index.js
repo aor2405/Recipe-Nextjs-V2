57ea7ef42dd5bcc98fc7f0a681f5e77631261891
aor2405 <a.o-r@hotmail.com>
2022-05-16 12:39:10 +0400

    added responsive design

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import { Fragment } from 'react';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
-import { Popover, Transition } from '@headlessui/react';
-import { XIcon } from '@heroicons/react/outline';
+import Logo from '../components/Logo/Logo';
 import NavBar from '../components/layout/mainNavigation';
 
 export default function Home() {
@@ -17,26 +15,30 @@ export default function Home() {
       <>
         <NavBar />
         <main>
-          <div className="h-screen sm:mt-16">
-            <div className="mx-auto max-w-7xl lg:px-8">
-              <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
-                  <div className="sm:pl-4 lg:py-24 w-full rounded-lg bg-burntOrange shadow">
-                    <h1 className="mt-4 text-4xl font-extrabold text-peach sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+          <div className="lg:h-screen">
+            <div className="mx-auto max-w-6xl lg:px-8">
+              <div className="flex justify-center my-8 xs:hidden">
+                <Logo />
+              </div>
+              <div className="mt-16 md:grid md:grid-cols-2 md:gap-8">
+                <div className="mx-auto max-w-md px-4 text-center sm:max-w-2xl sm:px-8 md:flex md:items-center lg:px-0 lg:text-left">
+                  <div className="w-full rounded-lg bg-burntOrange shadow lg:pl-4 lg:py-2">
+                    {/* <h1 className="text-peach font-extrabold pt-12 sm:mt-5 sm:text-2xl lg:text-5xl lg:mt-6 xl:text-6xl"> */}
+                    <h1 className="text-peach font-extrabold pt-12 text-3xl sm:mt-5 md:text-4xl lg:text-6xl">
                       A better way to store your Recipes
                     </h1>
-                    <p className="mt-4 sm:text-xl lg:text-lg xl:text-xl">
+                    <p className="mt-4 sm:text-base md:text-lg xl:text-xl">
                       Jump right in and explore our many recipes that have been
                       submitted from all around the world. Feel free to share
                       some of our own and comment of others!
                     </p>
                     <div className="mt-10 sm:mt-12">
-                      <div className="sm:flex">
-                        <div className="mt-3 sm:mt-0 sm:ml-3 bg-peach rounded-lg">
+                      <div className="flex justify-center pb-4">
+                        <div className="bg-peach w-48 mt-3 sm:mt-0 sm:ml-3 rounded-lg">
                           <Link href="/recipes">
                             <button
                               type="submit"
-                              className="block w-full py-3 px-4  rounded-lg font-mediumfocus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                              className="block w-full py-3 px-4 rounded-lg"
                             >
                               View Recipes
                             </button>
@@ -46,12 +48,13 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
-                  <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                <div className="invisible md:visible lg:m-0 md:relative">
+                  {/* <div className="relative"> */}
+                  <div className="mx-auto max-w-md px-4 lg:max-w-none lg:px-0">
                     <img
-                      className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none rounded-lg"
+                      className="w-full rounded-lg lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                       src="https://images.unsplash.com/photo-1601226809816-b8c32440158a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
-                      alt=""
+                      alt="Image of stir fry cooking"
                     />
                   </div>
                 </div>
